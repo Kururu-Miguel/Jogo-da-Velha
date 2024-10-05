4acbf41cfad9e966cf1119abcb1ae46cb9c38fc5
Luiz Miguel <justmike9139@gmail.com>
2024-10-05 19:02:06 -0400

    ajustes finais, fim do projeto

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from classes import *
 
 
 menu = Menu(['jogar', 'sair'])
-entrada = menu.chamar()
-if entrada == 1:
-	velha = Velha()
-	velha.jogar()
-elif entrada == 2:
-	print('Saindo...')
-	input('>')
-	exit()
+while True:
+	entrada = menu.chamar()
+	if entrada == 1:
+		velha = Velha()
+		velha.jogar()
+	elif entrada == 2:
+		print('Saindo...')
+		input('>')
+		exit()
 
